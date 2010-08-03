d19b9fa72a8f5dec388b13bea64fdb6b5a29b84f
Sam Thorogood <sam.thorogood@gmail.com>
2010-08-03 12:20:24 +1000

    Removed old expansion code

--- sre2/regexp.go
+++ sre2/regexp.go
@@ -811,25 +811,7 @@ func cleanup(prog []*instr) []*instr {
 // Generates a simple, straight-forward NFA. Matches an entire regexp from the
 // given input string.
 func Parse(src string) (r *sregexp) {
-
-  if true {
-    // TODO: reenable this when submatch handles LR matches
-    src = ".*?(" + src + ").*?"
-  } else {
-    // possibly expand this RE all the way to the left
-    if len(src) > 0 && src[0] == '^' {
-      src = "(" + src[1:len(src)]
-    } else {
-      src = ".*?(" + src
-    }
-
-    // possibly expand this RE to the right
-    if src[len(src)-1] == '$' {
-      src = src[0:len(src)-1] + ")"
-    } else {
-      src = src + ").*?"
-    }
-  }
+  src = ".*?(" + src + ").*?"
 
   re := &sregexp{make([]*instr, 0, 1), 0}
   p := parser{re, src, -1, -1, 0}
