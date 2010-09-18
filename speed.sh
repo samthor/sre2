e8d808bc19fa33466ac2a3b509775d1c86c427fc
Sam Thorogood <sam.thorogood@gmail.com>
2010-09-18 15:55:05 +1000

    default values for ./speed.sh

--- speed.sh
+++ speed.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Simple speed test for SRE2.
 
+RE="(a|(b))+"
+STR="aba"
+RUNS=100000
+
+CMD="./main -runs=$RUNS -re=$RE -s=$STR"
+
 make
 if [ $? != 0 ]; then
   exit $?
@@ -8,16 +14,16 @@ fi
 echo
 
 echo "==sre2 simple (fast, no submatches and uses bitset for states)"
-time -p ./main 2>/dev/null >/dev/null
+time -p $CMD 2>/dev/null >/dev/null
 echo
 
 echo "==sre2 submatch (slow, abuse of gc for states)"
-time -p ./main -sub 2>/dev/null >/dev/null
+time -p $CMD -sub 2>/dev/null >/dev/null
 echo
 
 # NB: Go's regexp module has the same underlying code whether we care about
 # submatches or not. This might change in the future.
 echo "==go regexp (medium speed, always cares about submatches with good gc)"
-time -p ./main -m >/dev/null
+time -p $CMD -m >/dev/null
 echo
 
