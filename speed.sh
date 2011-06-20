36fb45452d6b66bebe9bd5e3dc49727c2fd0463c
Sam Thorogood <sam.thorogood@gmail.com>
2011-06-20 14:24:57 +1000

    fixes to speed as well as sample runner (quotes/spaces are bad for bash)

--- speed.sh
+++ speed.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Simple speed test for SRE2.
 
-RE=".*(a|(b))+(\t*).+"
-STR="aba\thello"
+RE=".*(a|(b))+(#*).+"
+STR="aba#hello"
 RUNS=100000
 
-CMD="./main -runs=$RUNS -re=\"$RE\" -s=\"$STR\""
+CMD="./main -runs=$RUNS -re=$RE -s=$STR"
 
 make
 if [ $? != 0 ]; then
@@ -16,19 +16,19 @@ echo "RUNS=$RUNS RE=$RE STR=$STR"
 echo "CMD=$CMD"
 echo
 
-# On 2.53ghz Core 2 Duo: ~1.20
+# On 2.13ghz Core 2 Duo: ~1.30
 echo "==sre2 simple (fast, no submatches and uses bitset for states)"
 time -p $CMD #2>/dev/null >/dev/null
 echo
 
-# On 2.53ghz Core 2 Duo: ~2.00
+# On 2.13ghz Core 2 Duo: ~2.00
 echo "==sre2 submatch (slow, abuse of gc for states)"
 time -p $CMD -sub #2>/dev/null >/dev/null
 echo
 
 # NB: Go's regexp module has the same underlying code whether we care about
 # submatches or not. This might change in the future.
-# On 2.53ghz Core 2 Duo: ~4.05
+# On 2.53ghz Core 2 Duo: ~2.25
 echo "==go regexp (medium speed, always cares about submatches with good gc)"
 time -p $CMD -sub -m #>/dev/null
 echo
