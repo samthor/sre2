1ea293765bb6c01f720f3893c63f480d55cacce9
Sam Thorogood <sam.thorogood@gmail.com>
2011-06-20 14:26:07 +1000

    typo in speeds

--- speed.sh
+++ speed.sh
@@ -16,6 +16,7 @@ echo "RUNS=$RUNS RE=$RE STR=$STR"
 echo "CMD=$CMD"
 echo
 
+# Test machine is a late-2010, 13" MacBook Air.
 # On 2.13ghz Core 2 Duo: ~1.30
 echo "==sre2 simple (fast, no submatches and uses bitset for states)"
 time -p $CMD #2>/dev/null >/dev/null
@@ -28,7 +29,7 @@ echo
 
 # NB: Go's regexp module has the same underlying code whether we care about
 # submatches or not. This might change in the future.
-# On 2.53ghz Core 2 Duo: ~2.25
+# On 2.13ghz Core 2 Duo: ~2.25
 echo "==go regexp (medium speed, always cares about submatches with good gc)"
 time -p $CMD -sub -m #>/dev/null
 echo
